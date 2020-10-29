9666527deaf7688599abc032f72e35c911198db3
Biswayan Paul <biswayanpaul2015@gmail.com>
2020-10-29 10:09:06 +0530

    Update 1stProgramme.cpp

--- 1stProgramme.cpp
+++ 1stProgramme.cpp
@@ -11,7 +11,7 @@ int main(){
     int ammount2;
     cin>>ammount2;
     int sum=ammount1+ammount2;
-    cout<<"sum\n";//If we write sum inside quotetions then it will print the word only and not the value stored in it
+    cout<<"sum"<<endl;//If we write sum inside quotetions then it will print the word only and not the value stored in it
 
     cout<<sum<<endl;//this is the correct way to print the value stored in sum
     //<<endl refers to next line or we can use\n for new line
